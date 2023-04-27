4bf24bfd3190be9bd5c4086b230fdc5cb1201561
Rikusen <arigatoriku@aol.com>
2023-04-27 14:13:17 -0500

    Position type is complete.

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -13,6 +13,11 @@ public class Location {
         nextId++;
     }
 
+    public Location(String value) {
+        this();
+        this.value = value;
+    }
+
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
